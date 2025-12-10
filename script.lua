95ed270fa27919e46d9173f79a0c872d9823f481
ZKAY404 <baovq.tiger@gmail.com>
2025-12-10 08:07:07 +0700

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,3 @@
+getgenv().SEARCH_PLACE = 129009554587176 -- World 1: 76558904092080 World 2: 129009554587176
+getgenv().TARGET_PLACE = 129009554587176
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZKAY404/AutoFindServerTheForge/refs/heads/main/main.lua"))()
